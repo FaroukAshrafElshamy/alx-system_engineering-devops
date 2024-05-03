f83e14b71e33114157e5b720b910f2f32579a304
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-05-04 00:18:13 +0300

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,6 @@
-# Use pip3 to install flask with a specific version
-
-# Ensure python is installed
-class { 'python': }
+# Using Puppet, install puppet-lint
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
