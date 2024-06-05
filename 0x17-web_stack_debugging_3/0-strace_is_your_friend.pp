8dcb385b8b9ac6b78eb5749ad842805d326731b0
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-06-05 09:27:44 +0300

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixing a wordpress site
+exec { 'fix-wordpress':
+        command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+        /var/www/html/wp-settings.php; service apache2 restart"',
+        path    => '/usr/bin:/usr/sbin:/ibn'
+}
