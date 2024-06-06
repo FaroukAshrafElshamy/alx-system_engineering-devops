34c2a1b4e365558c5d8a330e761ce331dcb319bb
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-06-06 22:09:42 +0300

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,21 +2,21 @@
 """
 A module that returns the number of subscribers
 """
-
 import requests
 
 
 def get_subreddit_subscribers(subreddit):
     """Retrieves the number of subscribers in a given subreddit"""
     headers = {
+        'Accept': 'application/json',
         'User-Agent': ' '.join(['Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
                                 'AppleWebKit/537.36 (KHTML, like Gecko)',
-                                'Chrome/58.0.3029.110 Safari/537.3'])}
+                                'Chrome/58.0.3029.110',
+                                'Safari/537.3',
+                                'Edg/97.0.1072.62'])}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
+        return response.json()['data']['subscribers']
     return 0
